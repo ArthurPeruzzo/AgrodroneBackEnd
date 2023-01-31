12dcd2acdf02975c92b3ac64a3e61ac38fb6c7e3
ArthurPeruzzo <arthur.kohl@viasoft.com.br>
2023-01-31 19:57:10 -0300

    estrutura inical

---
+++ src/main/java/com/agrodrone/AgrodroneApplication.java
@@ -0,0 +1,13 @@
+package com.agrodrone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgrodroneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgrodroneApplication.class, args);
+	}
+
+}
